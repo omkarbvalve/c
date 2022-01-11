e5ff9f147c1dcb0eca87f7512d493321600b10e4
omkarbvalve <97109768+omkarbvalve@users.noreply.github.com>
2022-01-11 14:37:38 +0530

    Create in out.c

---
+++ in out.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   //input output
+   int a;
+   char b;
+   printf("enter anything \n");
+   scanf("%d \t %c",&a, &b);
+   printf("you entered %d %c",a ,b);
+   //backslash code in code
+   printf("\a -ring alert  \? question mark etc \f -form feed \o  octal \v vertical tab \thorizontal tab \r carriagereturn");
+}
